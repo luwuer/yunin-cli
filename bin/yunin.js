614e2584e0d4bf469997fd367204ac92500524ea
luwuer <html6@foxmail.com>
2019-02-05 13:03:21 +0800

    从package.json中取版本号

--- bin/yunin.js
+++ bin/yunin.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 const program = require('commander')
+const version = require('../package.json').version
 
 /**
  * commander命令解释：
  *   commander支持git风格的子命令处理，可以根据子命令自动引导到以特定格式命名的命令执行文件，格式是[command]-[subcommand]
  *   如yunin hello可以找到/bin/yunin-hello.js
  */
-
-program.version('1.0.0', '-v, --version')
+program.version(version, '-v, --version')
   .usage('<command>')
   .command('init', 'init a new project')
   .command('list', 'show template list')
