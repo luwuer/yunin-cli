cb6b75ea525d9d6f8b5e741200c03d9e051f0258
luwuer <html6@foxmail.com>
2019-04-09 13:31:31 +0800

    修改模板git仓库地址为https

--- bin/yunin-list.js
+++ bin/yunin-list.js
@@ -3,7 +3,8 @@ const notice = require('../lib/notice')
 const templates = require('../templates.json')
 
 const templateList = getList(templates)
-
+console.log(templateList
+)
 notice.info(`\n${templateList.length} templates available:`)
 templateList.forEach(template => {
   notice.info(`  · ${template.name}: ${template.des}`)
